0e60662095c69e73d206e823bae80116ed1ca1bc
flash <andreivali29@gmail.com>
2023-01-27 16:21:34 +0200

    stop user from selecting more seats than he previously desired

--- src/ThirdFrame.java
+++ src/ThirdFrame.java
@@ -4,28 +4,35 @@ import java.util.ArrayList;
 
 public class ThirdFrame extends JavaSwing {
     public static void init() {
-        int seats = Integer.parseInt(selectedSeatsNumber);
+        int seatsAsked = Integer.parseInt(selectedSeatsNumber);
+        int maxSeats = 20;
+
         ArrayList<Integer> currentSeats = new ArrayList<Integer>();
-        for (int i = 0; i <= seats; i++) {
+        for (int i = 0; i <= maxSeats; i++) {
             JButton btn = new JButton(Integer.toString(i));
             frame.add(btn);
-            int finalI = i;
+            int seat = i;
             btn.addActionListener(ae -> {
-                boolean add = !currentSeats.contains(finalI);
-                // check if an element is in the ArrayList
+                // check if current seats array includes selected seat
+                boolean add = !currentSeats.contains(seat);
 
+                // If number of selected seats is great than desired seats number return null
+                if (currentSeats.size() == Integer.parseInt(selectedSeatsNumber)) {
+                    btn.setOpaque(false);
+                    return;
+                }
+
+                // check if an element is in the ArrayList
                 if (add) {
-                    currentSeats.add(finalI);
+                    currentSeats.add(seat);
                     btn.setOpaque(true);
                     btn.setBackground(Color.GREEN);
                 } else {
-                    currentSeats.remove(Integer.valueOf(finalI));
+                    currentSeats.remove(Integer.valueOf(seat));
                     btn.setBackground(Color.WHITE);
                 }
-
-                System.out.println(currentSeats);
              });
-            }
+        }
 
         // setting grid layout of 3 rows and 3 columns
         frame.setLayout(new GridLayout(3, 3));
